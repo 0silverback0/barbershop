207b2007d5b070cee9c3ba3d508fca8f4d12bd2f
SilverBack Gorilla <silverbackgorilla@SilverBacks-MacBook-Pro.local>
2023-10-07 01:15:00 -0400

    clean up

--- src/Booking.js
+++ src/Booking.js
@@ -1,33 +1,3 @@
-// import React from 'react';
-// import { InlineWidget } from 'react-calendly'; // Import the specific component
-
-// function Booking() {
-//     return (
-//         <div className="container mt-5">
-//             <div className="row">
-//                 <div className="col-md-8 col-12 offset-md-2">
-//                     <div className="card">
-//                         <div className="card-body p-0">
-//                             <h2 className="card-title">Book an Appointment</h2>
-//                             <p className="card-text text-sm">
-//                                 Choose a convenient time for your appointment.
-//                             </p>
-//                             <div className="calendly-container">
-//                                 <div className="custom-inline-widget">
-//                                     <InlineWidget url="https://calendly.com/fourmarz" />
-//                                     {/* Use InlineWidget component */}
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Booking;
-
 import React from 'react';
 
 function Booking() {
